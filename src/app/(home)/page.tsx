1c2e8504dffc8766b7f3bc827dbc8cdcec8252d6
Rafael Santos <santosrafaell187@gmail.com>
2026-01-21 19:32:51 -0300

    feat: finalize desktop dashboard layout with glassmorphism and sticky navigation

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,20 @@
 import { FilterButton } from "@/components/filter-button";
+import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { UserCard } from "@/components/user-card";
 import { CircleCheck } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="w-full max-w-5xl mx-auto h-3/4 bg-[#09090b] border border-orange-500 rounded-md p-4">
-      <div className="flex justify-between py-5">
+    <main className="flex flex-col gap-4 w-full h-[90vh] justify-between max-w-5xl lg:max-w-7xl mx-auto bg-zinc-900/40 border border-orange-500 rounded-md p-4">
+      <header className="flex justify-between py-5">
         <h1 className="text-zinc-50 text-5xl font-bold">
           Kwai <span className="text-orange-500">Score</span>
         </h1>
@@ -14,14 +24,32 @@ export default function Home() {
           </span>
           <span className="text-zinc-50">Google Drive Conectado</span>
         </div>
-      </div>
+      </header>
       <div className="">
         <div className="flex gap-2">
           <FilterButton label="Alta qualidade" isActive />
-          <FilterButton label="Alta qualidade" isActive={false} />
-          <FilterButton label="Alta qualidade" isActive={false} />
-          <FilterButton label="Alta qualidade" isActive={false} />
         </div>
+      </div>
+      <div className="flex-1 overflow-hidden">
+        <ScrollArea className="h-full rounded-md p-4">
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-2">
+            <UserCard />
+          </div>
+        </ScrollArea>
+      </div>
+      <div className="flex gap-4 text-zinc-50">
+        <Select>
+          <SelectTrigger className="w-full border-orange-500 ">
+            <SelectValue placeholder="Selecionar Mês" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectContent>
+        </Select>
+
+        <Input className="border-orange-500" placeholder="Pesquisar" />
       </div>
     </main>
   );
---
+++ src/components/user-card.tsx
@@ -0,0 +1,52 @@
+import { ThumbsUp, Users } from "lucide-react";
+import Image from "next/image";
+
+export const UserCard = () => {
+  return (
+    <article
+      className="space-y-3 gap-3 border p-4 backdrop-blur-md border-hairline border-white/10 hover:bg-zinc-900/60
+  hover:border-orange-500 rounded-md"
+    >
+      <div className="flex justify-between items-start">
+        <div className="flex gap-3">
+          <Image
+            className="rounded-full border-2 border-orange-500"
+            src={"/logo-test.jpg"}
+            alt="user"
+            width={70}
+            height={70}
+          />
+          <div className="self-center text-zinc-50">
+            <h3 className="font-bold uppercase text-[0.7rem]">
+              Mma fight zone
+            </h3>
+            <h3 className="lowercase text-[0.6rem]">@mmafightzone</h3>
+          </div>
+        </div>
+        <div className="flex items-center bg-green-800 p-1 rounded-md">
+          <span className="text-[0.6rem] text-zinc-50">Alta Qualidade</span>
+        </div>
+      </div>
+      <footer className="mx-auto w-2/3 border border-hairline border-white/10 p-3 rounded-md">
+        <div className="flex justify-between text-zinc-50">
+          <div className="flex-col flex text-center  gap-1">
+            <span className="text-zinc-400 text-[0.8rem]">Seguidores</span>
+
+            <div className="flex gap-1 items-center">
+              <Users size={18} className="text-orange-500" />
+              <span className="text-amber-50 font-bold">963.2K</span>
+            </div>
+          </div>
+          <div className="flex-col flex text-cente  gap-1">
+            <span className="text-zinc-400 text-[0.8rem]">Curtidas</span>
+
+            <div className="flex gap-1  items-center">
+              <ThumbsUp size={18} className="text-orange-500" />
+              <span className="text-amber-50 font-bold">20.5M</span>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </article>
+  );
+};
