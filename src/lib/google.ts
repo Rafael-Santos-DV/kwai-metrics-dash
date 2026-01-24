42a0bcd4c5db627b3822593b16923c8255e9ad1f
Rafael Santos <santosrafaell187@gmail.com>
2026-01-24 18:46:41 -0300

    feat(lib): setup google auth with service account for drive and sheets

---
+++ src/lib/google.ts
@@ -0,0 +1,30 @@
+import { google } from "googleapis";
+
+// const oauth2Client = new google.auth.OAuth2({
+//   client_id: process.env.GOOGLE_CLIENT_ID,
+//   client_secret: process.env.GOOGLE_CLIENT_SECRET,
+//   redirectUri: process.env.GOOGLE_REDIRECT_URL,
+// });
+
+const auth = new google.auth.GoogleAuth({
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    client_secret: process.env.GOOGLE_CLIENT_SECRET,
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
+  },
+  scopes: [
+    "https://www.googleapis.com/auth/spreadsheets.readonly",
+    "https://www.googleapis.com/auth/drive.readonly",
+  ],
+});
+
+export const drive = google.drive({
+  version: "v3",
+  auth,
+});
+
+export const sheet = google.sheets({
+  version: "v4",
+  auth,
+});
